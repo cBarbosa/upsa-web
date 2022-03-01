72ecf33c92406e23721427d5d2e5791a5e82770b
Charles Barbosa <xbrown@gmail.com>
2022-03-01 12:11:55 -0300

    axios header config

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,18 +1,29 @@
 import axios from 'axios';
 
-function getAPIClient(ctx?: any) {
+function getAPIClient() {
 
     const AURUM_AUTH = process.env.NEXT_PUBLIC_THEMIS_TOKEN ?? '';
 
     const api = axios.create({
         baseURL: 'https://upsa.themisweb.penso.com.br/upsa/api',
-        headers: {'X-Aurum-Auth': AURUM_AUTH}
+        headers: {
+            // 'Access-Control-Allow-Origin': '*',
+            // // 'Access-Control-Allow-Headers': 'Content-Type',
+            // 'Content-Type': 'application/json;charset=utf-8',
+            // 'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+            // 'Access-Control-Allow-Headers': 'append,delete,entries,foreach,get,has,keys,set,values,Authorization',
+            // 'Host': 'upsa.themisweb.penso.com.br',
+            'X-Aurum-Auth': AURUM_AUTH
+        }
     });
 
-    api.interceptors.request.use(config => {
-        return config;
-    });
+    // api.interceptors.request.use(config => {
+    //     return config;
+    // });
 
+    // api.defaults.headers.common['X-Aurum-Auth'] = AURUM_AUTH;
+    // api.defaults.headers.common['Content-Type'] = 'application/json;charset=utf-8';
+   
     return api;
 }
 
