d03f7a447609fc56336375039d83ad7bb3e92dff
Charles Barbosa <xbrown@gmail.com>
2023-02-01 01:50:12 -0300

    feat: change service endpoint

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 function getAPIClient() {
 
     const api = axios.create({
-        baseURL: 'https://upsa-api.smile.tec.br/api',
+        baseURL: 'https://upsa-api.azurewebsites.net/api',
         headers: {
             'Access-Control-Allow-Origin': '*',
             'Content-Type': 'application/json;charset=utf-8',
